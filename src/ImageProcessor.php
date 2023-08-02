f310ff0f4a115a4a3248c2d1c03764ea9a8e0e08
Usoroh Paius <paius.usoroh@gmail.com>
2023-08-02 13:49:45 +0600

    подключаю и использую Imagick

--- src/ImageProcessor.php
+++ src/ImageProcessor.php
@@ -2,11 +2,19 @@
 
 namespace src;
 
+use Imagick;
+
 class ImageProcessor
 {
-    public function processImage($url, $size, $cropping){
+    public function processImage($url, $size, $cropping): Imagick
+    {
         $imageData = file_get_contents($url);
-
-        return $imageData;
+        $image = new Imagick();
+        $image->readImageBlob($imageData);
+        $dimensions = explode("x", $size);
+        $height = $dimensions[0];
+        $width = $dimensions[1];
+        $image->thumbnailImage($width, $height);
+        return $image;
     }
 }
