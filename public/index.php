296772bdcf8c498b82c0e11b6a3cb118ff4d21f7
Usoroh Paius <paius.usoroh@gmail.com>
2023-08-02 12:48:24 +0600

    подключаю и тестирую классы

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
-header('Content-type: application/json; charset=utf-8');
-echo json_encode(['status' => 'ok', 'message' => 'Kolesa Academy!']);
+
+require_once __DIR__ . '/../src/RequestHandler.php';
+require_once __DIR__ . '/../src/ImageProcessor.php';
+
+use src\ImageProcessor;
+use src\RequestHandler;
+
+try {
+    $handler = new RequestHandler();
+    $handler->validate();
+
+    $processor = new ImageProcessor();
+    $image = $processor->processImage($handler->getUrl(), $handler->getSize(), $handler->getCropping());
+
+    header('Content-Type: image/jpeg');
+    echo $image;
+} catch (Exception $e) {
+    header('Content-type: application/json; charset=utf-8');
+    http_response_code(400);
+    echo json_encode(['error' => $e->getMessage()]);
+}
